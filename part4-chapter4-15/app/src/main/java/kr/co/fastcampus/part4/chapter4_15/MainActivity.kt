3490fac661c3a7ec7458b0bb838c9d7dd75d629b
dreamboy1339 <20683340+dreamboy1339@users.noreply.github.com>
2024-03-10 14:24:15 +0900

    :construction: Part4 chapter4-15 animation-2

--- part4-chapter4-15/app/src/main/java/kr/co/fastcampus/part4/chapter4_15/MainActivity.kt
+++ part4-chapter4-15/app/src/main/java/kr/co/fastcampus/part4/chapter4_15/MainActivity.kt
@@ -3,18 +3,28 @@ package kr.co.fastcampus.part4.chapter4_15
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.Crossfade
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.animateColor
 import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.RadioButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -39,49 +49,148 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun Animation2Ex() {
     var isDarkMode by remember { mutableStateOf(false) }
 
-    // 단계 1: `updateTransition` 수행하고 `targetState`를 `isDarkMode`로
-    // 설정합니다. `transition`으로 리턴을 받습니다.
+    // 단계 1: `updateTransition` 수행하고 `targetState`를 `isDarkMode`로 설정합니다. `transition`으로 리턴을 받습니다.
+
+    val transition = updateTransition(
+        targetState = isDarkMode,
+        label = "myTransition"
+    )
 
     // 단계 2: `transition`에 대해 `animateColor`를 호출해 `backgroundColor`를 받습니다.
     // 배경색상을 만듭시다. false일 때 하얀 배경, true일 때 검은 배경.
 
+    val backgroundColor by transition.animateColor(label = "myColorAnimation") {
+        when (it) {
+            true -> Color.Black
+            else -> Color.White
+        }
+    }
+
     // 단계 3: 글자 색상을 만듭시다.
+    val color by transition.animateColor(label = "myColorAnimation") {
+        when (it) {
+            true -> Color.White
+            else -> Color.Black
+        }
+    }
 
     // 단계 4: `animateFloat`를 호출해서 알파 값을 만듭시다.
+    val alpha by transition.animateFloat(label = "dark mode alpha animation") {
+        when (it) {
+            true -> 1.0f
+            else -> 0.5f
+        }
+    }
 
     // 단계 5: 컬럼에 배경과 알파를 적용합시다.
-    Column {
+    Column(
+        modifier = Modifier
+            .background(backgroundColor)
+            .alpha(alpha)
+    ) {
         // 단계 6: 라디오 버튼에 글자 색을 적용합시다.
-        RadioButtonWithText(text = "일반 모드", selected = !isDarkMode) {
+        RadioButtonWithText(
+            text = "일반 모드",
+            selected = !isDarkMode,
+            color = color,
+        ) {
             isDarkMode = false
         }
-        RadioButtonWithText(text = "다크 모드", selected = isDarkMode) {
+        RadioButtonWithText(
+            text = "다크 모드",
+            selected = isDarkMode,
+            color = color,
+        ) {
             isDarkMode = true
         }
 
         // 단계 7: Crossfade를 이용해 `isDarkMode`가 참일 경우
         // `Row`로 항목을 표현하고 거짓일 경우 `Column`으로 표현해봅시다.
-        Row {
-            Box(modifier = Modifier
-                .background(Color.Red)
-                .size(20.dp)) {
-                Text("1")
-            }
-            Box(modifier = Modifier
-                .background(Color.Magenta)
-                .size(20.dp)) {
-                Text("2")
-            }
-            Box(modifier = Modifier
-                .background(Color.Blue)
-                .size(20.dp)) {
-                Text("3")
+        Crossfade(targetState = isDarkMode) {
+            when (it) {
+                false -> {
+                    Column {
+                        Box(
+                            modifier = Modifier
+                                .background(Color.Red)
+                                .size(20.dp)
+                        ) {
+                            Text("1")
+                        }
+                        Box(
+                            modifier = Modifier
+                                .background(Color.Magenta)
+                                .size(20.dp)
+                        ) {
+                            Text("2")
+                        }
+                        Box(
+                            modifier = Modifier
+                                .background(Color.Blue)
+                                .size(20.dp)
+                        ) {
+                            Text("3")
+                        }
+                    }
+
+                }
+
+                true -> {
+                    /*Row {
+                        Box(
+                            modifier = Modifier
+                                .background(Color.Red)
+                                .size(20.dp)
+                        ) {
+                            Text("1")
+                        }
+                        Box(
+                            modifier = Modifier
+                                .background(Color.Magenta)
+                                .size(20.dp)
+                        ) {
+                            Text("2")
+                        }
+                        Box(
+                            modifier = Modifier
+                                .background(Color.Blue)
+                                .size(20.dp)
+                        ) {
+                            Text("3")
+                        }
+                    }*/
+                    Text(text = "blablbalbal", color = color)
+                }
             }
         }
+//        Row {
+//            Box(
+//                modifier = Modifier
+//                    .background(Color.Red)
+//                    .size(20.dp)
+//            ) {
+//                Text("1")
+//            }
+//            Box(
+//                modifier = Modifier
+//                    .background(Color.Magenta)
+//                    .size(20.dp)
+//            ) {
+//                Text("2")
+//            }
+//            Box(
+//                modifier = Modifier
+//                    .background(Color.Blue)
+//                    .size(20.dp)
+//            ) {
+//                Text("3")
+//            }
+//        }
     }
 }
 
