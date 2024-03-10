4022b22d209698a478a4f3302d73e06346c50e3b
dreamboy1339 <20683340+dreamboy1339@users.noreply.github.com>
2024-03-10 12:37:37 +0900

    :construction: Part4 chapter4-13 state & state hoisting

--- part4-chapter4-13/app/src/main/java/kr/co/fastcampus/part4/chapter4_13/MainActivity.kt
+++ part4-chapter4-13/app/src/main/java/kr/co/fastcampus/part4/chapter4_13/MainActivity.kt
@@ -10,7 +10,11 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -34,38 +38,90 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun PyeongToSquareMeter() {
-    var pyeong by remember {
+    var pyeong by rememberSaveable {
         mutableStateOf("23")
     }
-    var squaremeter by remember {
+    var squaremeter by rememberSaveable {
         mutableStateOf((23 * 3.306).toString())
     }
+
+//    rememberScaffoldState()
 
     // 단계 1: remember를 이용해 상태를 만들고 평 값을 입력하면
     // 제곱미터가 출력되도록 화면을 구성하시오.
     // 평을 제곱미터로 바꾸기 위해서는 3.306을 곱하면 됩니다.
-    Column(modifier = Modifier.padding(16.dp)) {
-        OutlinedTextField(
-            value = "",
-            onValueChange = {
-            }, label = {
-                Text("평")
-            }
-        )
-        OutlinedTextField(
-            value = "",
-            onValueChange = {},
-            label = {
-                Text("제곱미터")
-            }
-        )
-    }
+//    Column(modifier = Modifier.padding(16.dp)) {
+//        OutlinedTextField(
+//            value = pyeong,
+//            onValueChange = {
+//                if (it.isBlank()) {
+//                    pyeong = ""
+//                    squaremeter = ""
+//                    return@OutlinedTextField
+//                }
+//                val numericValue = it.toFloatOrNull() ?: return@OutlinedTextField
+//                pyeong = it
+//                squaremeter = (numericValue * 3.306).toString()
+//            }, label = {
+//                Text("평")
+//            }
+//        )
+//        OutlinedTextField(
+//            value = squaremeter,
+//            onValueChange = {
+//                squaremeter = it
+//            },
+//            label = {
+//                Text("제곱미터")
+//            }
+//        )
+//    }
 
     // 단계 2: `Composable` 함수를 만들고 `Column`의 항목들을 옮기세요.
     // 단 상태는 옮기지 말아야 합니다.
 
     // 파라미터는 아래와 같이 구성합니다.
     // `pyeong: String, squareMeter: String, onPyeongChange: (String) -> Unit`
+
+    PyeongToSqureMeterStateless(
+        pyeong = pyeong,
+        squareMeter = squaremeter
+    ) {
+        if (it.isBlank()) {
+            pyeong = ""
+            squaremeter = ""
+            return@PyeongToSqureMeterStateless
+        }
+        val numericValue = it.toFloatOrNull() ?: return@PyeongToSqureMeterStateless
+        pyeong = it
+        squaremeter = (numericValue * 3.306).toString()
+    }
+}
+
+@Composable
+fun PyeongToSqureMeterStateless(
+    pyeong: String,
+    squareMeter: String,
+    onPyeongChange: (String) -> Unit
+) {
+    Column(modifier = Modifier.padding(16.dp)) {
+        OutlinedTextField(
+            value = pyeong,
+            onValueChange = {
+                onPyeongChange(it)
+
+            }, label = {
+                Text("평")
+            }
+        )
+        OutlinedTextField(
+            value = squareMeter,
+            onValueChange = {},
+            label = {
+                Text("제곱미터")
+            }
+        )
+    }
 }
 
 @Preview(showBackground = true)
