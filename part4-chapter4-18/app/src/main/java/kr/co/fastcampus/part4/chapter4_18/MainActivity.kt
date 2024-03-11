8066d16c3165b1debc1a2d9644baf42c940c338c
dreamboy1339 <20683340+dreamboy1339@users.noreply.github.com>
2024-03-11 23:10:30 +0900

    :construction: Make Todo

--- part4-chapter4-18/app/src/main/java/kr/co/fastcampus/part4/chapter4_18/MainActivity.kt
+++ part4-chapter4-18/app/src/main/java/kr/co/fastcampus/part4/chapter4_18/MainActivity.kt
@@ -4,11 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.Crossfade
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.Checkbox
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -39,11 +56,53 @@ fun TopLevel() {
     // 단계 4: `onSubmit`, `onEdit`, `onToggle`, `onDelete`를
     // 만들어 `ToDo`에 연결합니다.
 
+    val onSubmit: (String) -> Unit = {
+        val key = (toDoList.lastOrNull()?.key ?: 0) + 1
+        val todo = ToDoData(
+            key = key,
+            text = it,
+            done = false
+        )
+        toDoList.add(todo)
+        setText("")
+    }
+
+    val onToggle: (Int, Boolean) -> Unit = { key, checked ->
+        val i = toDoList.indexOfFirst { it.key == key }
+        toDoList[i] = toDoList[i].copy(done = checked)
+    }
+
+    val onDelete: (Int) -> Unit = { key ->
+        val i = toDoList.indexOfFirst { it.key == key }
+        toDoList.removeAt(i)
+    }
+
+    val onEdit: (Int, String) -> Unit = { key, newText ->
+        val i = toDoList.indexOfFirst { it.key == key }
+        toDoList[i] = toDoList[i].copy(text = newText)
+    }
+
     Scaffold {
         Column {
-            ToDoInput(text, setText, {})
+            ToDoInput(text, setText, onSubmit)
             // 단계 3: `LazyColumn`으로 `toDoList`를 표시합시다.
             // `key`를 `toDoData`의 `key`를 사용합니다.
+
+            LazyColumn {
+                items(
+                    toDoList,
+                    key = {
+                        it.key
+                    }
+                ) { toDoData ->
+                    ToDo(
+                        toDoData = toDoData,
+                        onToggle = onToggle,
+                        onDelete = onDelete,
+                        onEdit = onEdit
+                    )
+                }
+            }
         }
     }
 }
@@ -93,6 +152,7 @@ fun ToDo(
     onDelete: (key: Int) -> Unit = {}
 ) {
     var isEditing by remember { mutableStateOf(false) }
+
     Card(
         modifier = Modifier.padding(4.dp),
         elevation = 8.dp
@@ -101,9 +161,71 @@ fun ToDo(
         // 단계 1: `Row`를 만들고 `toDoData.text`를 출력하고
         // 완료를 체크하는 체크박스, 수정 버튼, 삭제 버튼을 만드세요.
 
+
         // 단계 2: `Crossfade`를 통해 `isEditing`을 따라 다른
         // UI를 보여줍니다. `OutlinedTextField`와 `Button을
         // 넣어봅시다.
+        Crossfade(targetState = isEditing) {
+            when (it) {
+                false -> {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        Text(
+                            text = toDoData.text,
+                            modifier = Modifier.weight(1f)
+                        )
+
+                        Text(text = "Complete")
+
+                        Checkbox(
+                            checked = toDoData.done,
+                            onCheckedChange = { checked ->
+                                onToggle(toDoData.key, checked)
+                            }
+                        )
+
+                        Button(onClick = {
+                            isEditing = true
+                        }) {
+                            Text(text = "Modify")
+                        }
+
+                        Spacer(modifier = Modifier.size(4.dp))
+
+                        Button(onClick = {
+                            onDelete(toDoData.key)
+                        }) {
+                            Text(text = "Delete")
+                        }
+                    }
+                }
+
+                true -> {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        val (newText, setNewText) = remember {
+                            mutableStateOf(toDoData.text)
+                        }
+                        OutlinedTextField(
+                            value = newText,
+                            onValueChange = setNewText,
+                            modifier = Modifier.weight(1f)
+                        )
+                        Spacer(modifier = Modifier.size(4.dp))
+                        Button(onClick = {
+                            isEditing = false
+                            onEdit(toDoData.key, newText)
+                        }) {
+                            Text(text = "Complete")
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
